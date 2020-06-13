e2e57e9de4ac5f69437ce4a22876046e261ccc4b
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-13 10:11:48 -0500

    Revert "implement push, pop and shift"
    This reverts commit fb684ea0124017c6aa018d289fb56d3272620745.

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -3,82 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-
-  getValue() {
-    return this.value;
-  }
-
-  getNext() {
-    return this.next;
-  }
-
-  setNext(newNext) {
-    this.next = newNext;
-  }
-}
-
 export class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  push(value) {
-    const newNode = new Node(value);
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.setNext(newNode);
-      this.tail = newNode;
-    }
+  push() {
+    throw new Error('Remove this statement and implement this function');
   }
 
   pop() {
-    if (this.head === null) {
-      return null;
-    }
-
-    if (Object.is(this.head, this.tail)) {
-      const value = this.head.getValue();
-      this.head = null;
-      this.tail = null;
-      return value;
-    }
-
-    let current = this.head;
-
-    while (current.getNext() !== this.tail) {
-      current = current.getNext();
-    }
-
-    let value = this.tail.getValue();
-    this.tail = current;
-
-    return value;
+    throw new Error('Remove this statement and implement this function');
   }
 
   shift() {
-    if (this.head === null) {
-      return null;
-    }
-
-    if (!this.head.getNext()) {
-      const head = this.head;
-      this.head = null;
-      this.tail = null;
-      return head.getValue();
-    }
-
-    const value = this.head.getValue();
-    this.head = this.head.getNext();
-
-    return value;
+    throw new Error('Remove this statement and implement this function');
   }
 
   unshift() {
