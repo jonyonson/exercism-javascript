6b8b3ee3fe57be9cb4b392d716283aa8d671b1ce
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-13 12:33:29 -0500

    working solution

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -44,13 +44,13 @@ export class LinkedList {
 
   pop() {
     const value = this.tail.value;
-    this.delete(this.tail);
+    this.deleteNode(this.tail);
     return value;
   }
 
   shift() {
     const value = this.head.value;
-    this.delete(this.head);
+    this.deleteNode(this.head);
     return value;
   }
 
@@ -69,7 +69,51 @@ export class LinkedList {
     this.length++;
   }
 
-  delete(node) {
+  delete(value) {
+    if (value instanceof Node) {
+      this.deleteNode(node);
+      return;
+    }
+    let currentNode = this.head;
+
+    // only one value
+    if (!currentNode.next && currentNode.value === value) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return;
+    }
+
+    let valueFound = false;
+
+    if (value === this.head.value) {
+      this.head = this.head.next;
+      valueFound = true;
+    }
+
+    if (value === this.tail.value) {
+      this.tail = this.tail.previous;
+      valueFound = true;
+    }
+
+    while (currentNode.next && currentNode.next.value !== value) {
+      currentNode = currentNode.next;
+    }
+
+    if (currentNode.next && currentNode.next.value === value) {
+      if (currentNode.next.next) {
+        currentNode.next = currentNode.next.next;
+        currentNode.next.previous = currentNode;
+      } else {
+        currentNode.next = null;
+      }
+      valueFound = true;
+    }
+
+    valueFound && this.length--;
+  }
+
+  deleteNode(node) {
     this.length--;
 
     if (!this.head && !this.tail) return;
