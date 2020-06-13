9a532d9f6fb2503dd36ffd826f9af0cf10629991
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-06-13 10:24:39 -0500

    implement push

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -3,9 +3,34 @@
 // convenience to get you started writing code faster.
 //
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
 export class LinkedList {
-  push() {
-    throw new Error('Remove this statement and implement this function');
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.previous = this.tail;
+      this.tail = node;
+    }
+
+    this.length++;
   }
 
   pop() {
